bbd7a54ec0384017baad3a379a3a22576fffc7b6
Raphael da Silva Araújo <raphaelaraujotoc@outlook.com>
2022-01-21 19:50:56 -0300

    adicionando novas urls, class-based views e templates

--- catalog/urls.py
+++ catalog/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index')
+    path('', views.index, name='index'),
+    path('books/', views.BookListView.as_view(), name='books'),
+    path('book/<int:pk>', views.BookDetailView.as_view(), name='book-detail')
 ]
--- catalog/views.py
+++ catalog/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 from .models import Book, Author, BookInstance, Genre
+from django.views import generic
+
 
 # Create your views here.
 
@@ -10,19 +12,39 @@ def index(request):
     # Gera a contagem de alguns dos objetos principais
     num_books = Book.objects.all().count()
     num_instances = Book.objects.all().count()
+    num_genres = Genre.objects.all().count()
     
     # Livros disponíveis (status = 'a')
-    num_instances_avaiable = BookInstance.objects.filter(status__exact='a').count()
+    num_instances_available = BookInstance.objects.filter(status__exact='a').count()
+    
+    # Livro com alguma palavra específica:
+    word = 'Outro'
+    filter_books_by = Book.objects.filter(title__icontains=word)
     
     # O 'all()' fica implícito como padrão.
-    num_autors = Author.objects.count()
+    num_authors = Author.objects.count()
     
     context = {
         'num_books': num_books,
         'num_instances': num_instances,
-        'num_instances_avaiable': num_instances_avaiable,
-        'num_autors': num_autors
+        'num_instances_available': num_instances_available,
+        'num_authors': num_authors,
+        'num_genres': num_genres,
+        'word': word,
+        'filter_books_by': filter_books_by
     }
     
     # Renderiza o template HTML (index.html) com os dados da variável 'context':
     return render(request, 'index.html', context=context)
+
+
+class BookListView(generic.ListView):
+    model = Book
+    context_object_name = 'book_list'  # <- variável de template
+    queryset = Book.objects.filter(title__icontains='Livro')[:5]  # Pega 5 livros que contém no título a palavra 'Outro'
+    template_name = 'books/book_list.html'  # Especifica o nome/localização do template
+    paginate_by = 3
+
+
+class BookDetailView(generic.DetailView):
+    model = Book
