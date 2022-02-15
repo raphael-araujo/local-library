d6a3053130fb5c9a5b811442a9246684ba282998
Raphael da Silva Araújo <raphaelaraujotoc@outlook.com>
2022-02-15 19:28:00 -0300

    criando testes para o formulário

---
+++ catalog/tests/test_forms.py
@@ -0,0 +1,40 @@
+import datetime
+
+from django.test import SimpleTestCase
+from django.utils import timezone
+
+from ..forms import RenewBookForm
+
+
+class RenewBookFormTest(SimpleTestCase):
+    def test_renew_form_date_field_label(self):
+        form = RenewBookForm()
+        self.assertTrue(
+            form.fields['renewal_date'].label is None or form.fields['renewal_date'].label == 'renewal date'
+        )
+
+    def test_renew_form_date_field_help_text(self):
+        form = RenewBookForm()
+        self.assertEqual(
+            form.fields['renewal_date'].help_text, 'Insira uma data entre agora e 4 semanas (padrão 3 semanas).'
+        )
+
+    def test_renew_form_date_in_past(self):
+        date = datetime.date.today() - datetime.timedelta(days=1)
+        form = RenewBookForm(data={'renewal_date': date})
+        self.assertFalse(form.is_valid())
+
+    def test_renew_form_date_too_far_in_future(self):
+        date = datetime.date.today() + datetime.timedelta(weeks=4) + datetime.timedelta(days=1)
+        form = RenewBookForm(data={'renewal_date': date})
+        self.assertFalse(form.is_valid())
+
+    def test_renew_form_date_today(self):
+        date = datetime.date.today()
+        form = RenewBookForm(data={'renewal_date': date})
+        self.assertTrue(form.is_valid())
+
+    def test_renew_form_date_max(self):
+        date = timezone.localtime() + datetime.timedelta(weeks=4)
+        form = RenewBookForm(data={'renewal_date': date})
+        self.assertTrue(form.is_valid())
