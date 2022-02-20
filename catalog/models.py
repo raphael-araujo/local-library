18575c95dd648f0002d5ca6d6ab479b667695b74
Raphael da Silva Araújo <raphaelaraujotoc@outlook.com>
2022-02-19 23:49:40 -0300

    alterando o campo 'date_of_death' do model Author

--- catalog/models.py
+++ catalog/models.py
@@ -109,7 +109,7 @@ class Author(models.Model):
     first_name = models.CharField(max_length=100)
     last_name = models.CharField(max_length=100)
     date_of_birth = models.DateField(null=True, blank=True)
-    date_of_death = models.DateField('Died', null=True, blank=True)
+    date_of_death = models.DateField('died', null=True, blank=True)
     
     class Meta:
         ordering = ['last_name', 'first_name']
