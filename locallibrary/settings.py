f2393d63765680c48b5d29f5ad783b85ba005617
Raphael da Silva Araújo <raphaelaraujotoc@outlook.com>
2022-02-23 19:41:19 -0300

    alterando settings.py

--- locallibrary/settings.py
+++ locallibrary/settings.py
@@ -20,6 +20,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
+# SECURITY WARNING: keep the secret key used in production secret!
+# SECRET_KEY = 'django-insecure-cdmv6en3n6m73ynvsa=jd*id8bf5da-5x#tgwj3^)gv)(_tale'
+
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
